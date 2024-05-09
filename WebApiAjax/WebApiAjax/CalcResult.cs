dbd9069656e99dbfaabe355499e2f0d356b4d30f
JBeisert <97980832+JBeisert@users.noreply.github.com>
2024-05-09 10:40:35 +0200

    lab 7.1
    lab 7.1

---
+++ WebApiAjax/WebApiAjax/CalcResult.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiAjax
+{
+    public class CalcResult
+    {
+        public int Sum { get; set; }
+        public int Difference { get; set; }
+        public int Product { get; set; }
+        public int Quotient { get; set; }
+    }
+}
---
+++ WebApiAjax/WebApiAjax/Program.cs
@@ -0,0 +1,45 @@
+using WebApiAjax;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
+app.MapGet("/math",
+ (int x, int y) => new CalcResult()
+ {
+     Sum = x + y,
+     Difference = x - y,
+     Product = x * y,
+     Quotient = x / y
+ });
+
+app.UseStaticFiles();
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
